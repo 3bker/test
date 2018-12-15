874d4f7bc45da4186858a37da993a803440cbea0
3bker <44803452+3bker@users.noreply.github.com>
2018-12-15 08:27:28 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,43 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "A";
+
+                                   الاقتراحات كود 
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+  let command = message.content.split(" ")[0].slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+
+  if(!message.content.toLowerCase().startsWith(prefix)) return;
+  if(command == "suggest") {
+    if(!args.join(" ")) return message.reply(`${prefix}suggest <suggestion>`);
+    let channel = message.guild.channels.find(c => c.name == "suggestions");
+    let embed = new Discord.RichEmbed()
+    .setAuthor(message.author.username, message.author.displayAvatarURL)
+    .setTitle(`New Suggestion!`)
+    .setFooter(message.author.id)
+    .setDescription(args.join(" "));
+    channel.send(embed).then(msg => {
+      msg.react("✅").then(() => msg.react("❌"));
+      message.delete()
+      message.channel.send(`Success!, your suggestion has been recoded to <#${channel.id}>`);
+    });
+  }
+});
+
+
+
+
+
+
+
+client.on('guildMemberAdd', member=> {
+    member.addRole(member.guild.roles.find("name","اسم الرتبه"));
+    });
+
+
+client.login(process.env.BOT_TOKEN);
