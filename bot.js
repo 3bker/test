da420a96f27bc64ee5e61e82237ed819d46c2b84
3bker <44803452+3bker@users.noreply.github.com>
2018-12-17 01:08:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,4 +106,12 @@ client.on('message', message => {
 }); 
 
 
+
+client.on('message', message => {
+    if (message.content === 'zg') {
+        message.reply('pong');
+      }
+});
+
+
 client.login(process.env.BOT_TOKEN);
